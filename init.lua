6f0a6d2c55043ee3f1804dfff7b8da97a3923381
jrupinski <rupinskijakub@gmail.com>
2024-07-26 08:26:53 +0200

    feat: enable ruby formatter on save, fix timeout

--- init.lua
+++ init.lua
@@ -651,12 +651,13 @@ require('lazy').setup({
         -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true }
         return {
-          timeout_ms = 500,
+          timeout_ms = 1000,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
         }
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        ruby = { 'rubocop' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
