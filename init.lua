9065191278cb325a2b3e628c85a3e5cccee62e60
jrupinski <rupinskijakub@gmail.com>
2024-12-02 18:47:13 +0100

    feat: Revert fzf-lua to regular live grep

--- init.lua
+++ init.lua
@@ -424,15 +424,15 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      -- vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Use fuzzy search for live_grep
-      local builtin_fuzzy = require 'fzf-lua'
-      vim.keymap.set('n', '<leader>sg', builtin_fuzzy.live_grep_native, { desc = '[S]earch by [G]rep' })
+      -- local builtin_fuzzy = require 'fzf-lua'
+      -- vim.keymap.set('n', '<leader>sg', builtin_fuzzy.live_grep_native, { desc = '[S]earch by [G]rep' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
